4a5f10bac0c7bb403155065dfb142abe49d1c83f
Ahmed Magdy <ahmedmagdy41996@gmail.com>
2022-11-30 00:24:42 +0200

    Added Settings Loader Class

---
+++ Take Break 2/Take Break 2/SettingLoader/SettingModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Take_Break_2.SettingLoader
+{
+    public class SettingModel
+    {
+        public bool? Startup { get; set; }
+        public long? TotalSeconds { get; set; }
+        public long? WaitingTimeInSeconds { get; set; }
+    }
+}
---
+++ Take Break 2/Take Break 2/SettingLoader/SettingsLoader.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+using System.Text.Json.Nodes;
+using System.Net.Http.Json;
+using Newtonsoft.Json;
+
+namespace Take_Break_2.SettingLoader
+{
+    public class SettingsLoader
+    {
+        private readonly string FILE_NAME;
+
+        public SettingsLoader()
+        {
+            FILE_NAME = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "settings.json");
+        }
+
+        private void CreateFile()
+        {
+            using (var streamWriter = new StreamWriter(FILE_NAME))
+            {
+                streamWriter.WriteLine("{\"Startup\": true, \"TotalSeconds\": 1800, \"WaitingTimeInSeconds\": 900}");
+            }
+        }
+
+        private void EnsureSettingFileExists()
+        {
+            if (!File.Exists(FILE_NAME))
+                CreateFile();
+        }
+
+        public SettingModel LoadSettings()
+        {
+            EnsureSettingFileExists();
+            using (StreamReader sr = new StreamReader(FILE_NAME))
+            {
+                string settingsFileContent = sr.ReadToEnd();
+                var settings = JsonConvert.DeserializeObject<SettingModel>(settingsFileContent);
+                return settings;
+            }
+        }
+
+        public void SaveSettingItem(SettingModel settings)
+        {
+            EnsureSettingFileExists();
+            using (StreamWriter sw = new StreamWriter(FILE_NAME))
+            {
+                var settingsStr = JsonConvert.SerializeObject(settings);
+                sw.WriteLine(settingsStr);
+            }
+        }
+    }
+}
